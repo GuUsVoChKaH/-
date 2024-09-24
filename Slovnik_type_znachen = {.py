ab4bc1807937a43b64928b9ec6568ef09e05c467
SOROKA PAVLO <pavlosoroka30@gmail.com>
2024-09-24 23:30:33 +0300

    Add files via upload

---
+++ Slovnik_type_znachen = {.py
@@ -0,0 +1,12 @@
+Slovnik_type_znachen = {
+    "name": type("Pavlo"),
+    "age": type(16),
+    "contact_information": {
+        "email_adres": type("pavlosoroka30gmail.com")
+        "city": type("Lutsk")
+        "country": type('Ukraine')
+        "adress": type('Konyakina_5a')
+    },
+    "Is_student": type(True)
+}
+print(Slovnik_type_znachen)
---
+++ Словник.py
@@ -0,0 +1,12 @@
+Slovnik_znachennya = {
+    "name": 'Pavlo',
+    "age": 16,
+    "contact_information": {
+        "email_adres": 'pavlosoroka30gmail.com',
+        "phone_number": +380987882501,
+        "country": 'Ukraine',
+        "adress": 'Konyakina_5a',
+    },
+    "Is_student": True
+}
+print(Slovnik_znachennya)
